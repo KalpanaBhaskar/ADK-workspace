1b18c046679725e3f9b14a13f1a11bc74b53eb0c
KalpanaBhaskar <beingkb916@gmail.com>
2026-07-02 22:32:21 +0530

    math agent

--- my_first_agent/agent.py
+++ my_first_agent/agent.py
@@ -3,6 +3,8 @@ from google.adk.agents.llm_agent import Agent
 root_agent = Agent(
     model='gemini-3.5-flash',
     name='root_agent',
+    # OTHER agents read this to decide if they should delegate here
     description='A helpful assistant for user questions.',
+    # THIS agent reads this to know how to behave
     instruction='Answer user questions to the best of your knowledge',
 )
